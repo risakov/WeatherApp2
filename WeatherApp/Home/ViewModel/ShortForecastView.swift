0c162774a16ef5f4223a88ccdd2dd740d714bf7f
Anastasiya <nastya.lyg2004@gmail.com>
2024-09-30 21:25:51 +0400

    [IOS2-9] ShortForecastView-ThirdBlock-UPD

--- WeatherApp/Home/ViewModel/ShortForecastView.swift
+++ WeatherApp/Home/ViewModel/ShortForecastView.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ShortForecastView: UIView {
-
+    
     private lazy var stackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .vertical
@@ -16,7 +16,7 @@ class ShortForecastView: UIView {
         stackView.alignment = .leading
         return stackView
     }()
-
+    
     private func createLabel(text: String, fontSize: CGFloat = 16, weight: UIFont.Weight = .regular) -> UILabel {
         let label = UILabel()
         label.text = text
@@ -24,51 +24,53 @@ class ShortForecastView: UIView {
         label.textColor = .white
         return label
     }
-
-    private func createForecastRow(time: String, dayTemperature: Int, nightTemperature: Int) -> UIStackView {
-        let timeLabel = createLabel(text: time)
-        let dayTemperatureLabel = createLabel(text: "\(dayTemperature)°", fontSize: 20, weight: .medium)
-        let nightTepmeratureLabel = createLabel(text: "\(nightTemperature)°", fontSize: 20, weight: .medium)
-
-        let rowStackView = UIStackView(arrangedSubviews: [timeLabel, dayTemperatureLabel, nightTepmeratureLabel])
+    
+    struct WeatherOnTime {
+        let time: String
+        let dayTemperature: Int
+        let nightTemperature: Int
+    }
+    
+    private func createForecastRow(weather: WeatherOnTime) -> UIStackView {
+        let timeLabel = createLabel(text: weather.time)
+        let dayTemperatureLabel = createLabel(text: "\(weather.dayTemperature)°", fontSize: 20, weight: .medium)
+        let nightTemperatureLabel = createLabel(text: "\(weather.nightTemperature)°", fontSize: 20, weight: .medium)
+        
+        let rowStackView = UIStackView(arrangedSubviews: [timeLabel, dayTemperatureLabel, nightTemperatureLabel])
         rowStackView.axis = .vertical
         rowStackView.distribution = .equalSpacing
-
+        
         return rowStackView
     }
-
+    
     private func setupViews() {
         let titleLabel = createLabel(text: "1-DAY FORECAST (every 3 hours)", fontSize: 14)
         stackView.addArrangedSubview(titleLabel)
-
-        let forecastData: [(String, Int, Int)] = [
-            ("17:44", 15, 29),
-            ("20:44", 15, 29),
-            ("23:44", 15, 29)
-        ]
-
+        
+        let forecastData: [WeatherOnTime] = [
+            WeatherOnTime(time: "17:44", dayTemperature: 29, nightTemperature: 15),
+            WeatherOnTime(time: "20:44", dayTemperature: 29, nightTemperature: 15),
+            WeatherOnTime(time: "23:44", dayTemperature: 29, nightTemperature: 15)]
+        
         for data in forecastData {
-            let row = createForecastRow(time: data.0, dayTemperature: data.1, nightTemperature: data.2)
+            let row = createForecastRow(weather: data)
             stackView.addArrangedSubview(row)
         }
-
-        addSubview(stackView)
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            stackView.topAnchor.constraint(equalTo: topAnchor, constant: 16),
-            stackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
-            stackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
-            stackView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -16)
-        ])
     }
-
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupViews()
+        self.addSubview(stackView)
+        stackView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(16)
+            make.leading.equalToSuperview().offset(16)
+            make.trailing.equalToSuperview().offset(-16)
+            make.bottom.equalToSuperview().offset(-16)
+        }   
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
-
